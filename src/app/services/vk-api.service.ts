b41e85a647be0bebf2655f1fd3909ca863ef51a5
FedBashilov <fedbashilov@gmail.com>
2020-03-16 17:33:54 +0300

    add styles

--- src/app/services/vk-api.service.ts
+++ src/app/services/vk-api.service.ts
@@ -10,7 +10,9 @@ export class VkAPIService {
   public API_SERVER = "https://api.vk.com/method";
   public ACCESS_TOKEN = "4694dd9c243320930c26be49d551e44228a7b30643632bd84bf8d6f631994afb7ca44b71d966f3e7879a2";
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) {
+    //take token from cookies
+  }
 
   getFriends(): Observable<any>{
     return this.httpClient.get<any>(`${this.API_SERVER}/friends.search?count=5&fields=photo_100&access_token=${this.ACCESS_TOKEN}&v=5.52`);
